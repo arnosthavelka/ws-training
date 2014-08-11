ea55428cc8ce75be3e6e45285c82273fb413bac3
Arnošt Havelka <arnost.havelka@gmail.com>
2014-08-11 16:18:37 +0200

    + [ws-client] spring client - create request from ObjectFactory

--- ws-training-client/src/main/java/com/asseco/aha/training/ws/server/service/HelloService.java
+++
@@ -1,7 +0,0 @@
-package com.asseco.aha.training.ws.server.service;
-
-public interface HelloService {
-
-	public abstract String sayHello(String name);
-
-}
---
+++ ws-training-client/src/test/java/com/asseco/aha/training/ws/CalculatorRuntimeClient.java
@@ -0,0 +1,38 @@
+package com.asseco.aha.training.ws;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.asseco.aha.training.types.v1.AddRequest;
+import com.asseco.aha.training.types.v1.AddResponse;
+import com.asseco.aha.training.ws.v1.WsTrainingPortType;
+
+public class CalculatorRuntimeClient {
+
+	@Test
+	public void testCalc() throws MalformedURLException {
+		URL url = new URL("http://localhost:8080/ws-server-as/CalculatorWebService?WSDL");
+		QName qname = new QName("http://ws.server.ws.training.aha.asseco.com/", "CalculatorWebServiceService");
+
+		Service service = Service.create(url, qname);
+		WsTrainingPortType calc = service.getPort(WsTrainingPortType.class);
+
+		AddRequest ar = new AddRequest();
+		List<Integer> inputs = ar.getNumber();
+		inputs.add(5);
+		inputs.add(2);
+		inputs.add(1);
+
+		AddResponse response = calc.add(ar);
+		Assert.assertEquals(8, response.getResult());
+
+	}
+
+}
