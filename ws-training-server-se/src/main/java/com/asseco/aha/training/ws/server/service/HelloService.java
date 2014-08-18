4d215bdb266f889f14de7a68009bb318786916c3
Arnošt Havelka <arnost.havelka@gmail.com>
2014-08-18 10:06:13 +0200

    + [ws-training-server-se] formatting of web.xml II.

---
+++ ws-training-server-se/src/main/java/com/asseco/aha/training/ws/server/service/HelloService.java
@@ -0,0 +1,7 @@
+package com.asseco.aha.training.ws.server.service;
+
+public interface HelloService {
+
+	String sayHello(String name);
+
+}
---
+++ ws-training-server-se/src/main/java/com/asseco/aha/training/ws/server/ws/HelloWebService.java
@@ -0,0 +1,37 @@
+package com.asseco.aha.training.ws.server.ws;
+
+import javax.annotation.PostConstruct;
+import javax.jws.WebService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import com.asseco.aha.training.ws.server.service.HelloService;
+
+@WebService
+public class HelloWebService {
+
+	/**
+	 * Class logger
+	 */
+	private static final Logger LOG = LoggerFactory.getLogger(HelloWebService.class);
+
+	@Autowired
+	private HelloService service;
+
+	public String sayHello(String name) {
+		LOG.info("Starting hello WS method ...");
+		return service.sayHello(name);
+	}
+
+	/**
+	 * Spring initialization ...
+	 */
+	@PostConstruct
+	public void init() {
+		LOG.info("Spring initialization ...");
+		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+	}
+}
