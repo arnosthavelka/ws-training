008f4d1bf16826d7d049e5003e9a69113c3237f2
hh310 <arnost.havelka@deutsche-boerse.com>
2020-05-18 08:09:16 +0200

    #2 removal of @Autowired V1

--- ws-training-server-sb/src/main/java/com/asseco/aha/training/ws/server/ws/HelloRestService.java
+++ ws-training-server-sb/src/main/java/com/asseco/aha/training/ws/server/ws/HelloRestService.java
@@ -1,21 +1,24 @@
 package com.asseco.aha.training.ws.server.ws;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import com.asseco.aha.training.ws.server.service.HelloService;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 // http://localhost:8080/ws-server-sb/hello?name=Gabriel
 @RestController
+@RequiredArgsConstructor
 public class HelloRestService {
 
-    @Autowired
+	@NonNull
     private HelloService service;
 
-	@RequestMapping("/hello")
+	@GetMapping("/hello")
 	public String sayHello(@RequestParam String name) {
         SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
 
